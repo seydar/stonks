c2d36593d76edce48de5facb23f3757025fd7fac
Ari Brown <ari@aribrown.com>
2021-03-21 18:06:12 -0400

    renamed some scripts

--- script/build_splits.rb
+++ script/build_splits.rb
@@ -5,8 +5,19 @@ require './market.rb'
 url = proc {|ticker| "http://stocksplithistory.com/?symbol=#{ticker}" }
 nyse = Ticker.where(:exchange => 'NYSE').all
 
+# DB.drop_table :splits
+# 
+# DB.create_table? :splits do
+#   primary_key :id
+#   foreign_key :ticker_id, :tickers
+#   string :ratio
+#   datetime :date
+# 
+#   index :ticker_id
+# end
+
 nyse.each do |sym|
-  p sym.symbol
+  print "\nreviewing #{sym.symbol}: "
   doc = Nokogiri::HTML(URI.open(url[sym.symbol.downcase]))
   possibilities = doc.at 'td:contains("Split History Table")'
   table = possibilities.children[0]
@@ -14,10 +25,16 @@ nyse.each do |sym|
   trs = trs.map {|tr| tr.children.map {|td| td.text } }[1..-1] # strip the headers
   
   trs.each do |tds|
-    #puts Time.parse(DateTime.strptime(td[0].strip, "%m/%d/%Y").to_s)
+    date = Time.parse(DateTime.strptime(tds[0], "%m/%d/%Y").to_s
+
+    next if Split.where(:ticker_id => sym.id,
+                        :ratio     => tds[-1],
+                        :ex        => date).count > 0
+
     Split.create :ticker_id => sym.id,
                  :ratio     => tds[-1],
-                 :ex        => Time.parse(DateTime.strptime(tds[0], "%m/%d/%Y").to_s)
+                 :ex        => date)
+    print "."
   end
 end
 
