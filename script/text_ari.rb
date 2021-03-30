eb4d74567b6170b079f9b21d7d507f5ab390b103
Ari Brown <ari@aribrown.com>
2021-03-29 23:30:02 -0400

    got rid of the extra commas in the text

--- script/text_ari.rb
+++ script/text_ari.rb
@@ -1,7 +1,7 @@
 require '/home/ari/servers/auberge.rb'
 
 action = ARGV[0]
-stocks = ARGV[1..-1].join ", "
+stocks = ARGV[1..-1].join " "
 
 Auberge::Phone.sms :to => '16037297097',
                    :body => "#{action} #{stocks}"
