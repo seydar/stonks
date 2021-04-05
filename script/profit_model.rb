3145069c54d68ec5a29d6c91e78ee1250b1710b5
Ari Brown <ari@aribrown.com>
2021-04-05 14:58:13 -0400

    more work on the writeup. ready to send it to that financial advisor named james

--- script/profit_model.rb
+++ script/profit_model.rb
@@ -2,11 +2,11 @@ require 'pry'
 require './market.rb'
 require './script/helpers.rb'
 
-schemes = [{:m =>  0.00, :b => 0.6}]
+schemes = [{:m => -0.02,  :b => 5.2}]
 
 sim = Simulator.new
 sim.assessor.holding = (ARGV[0]..(ARGV[1] || ARGV[0])).inject([]) do |sum, year|
-  sum + holdings(:year => year, :drop => -0.2)
+  sum + holdings(:year => year, :drop => -0.3)
 end
 
 puts "loaded #{sim.assessor.holding.size} trxs"
@@ -21,14 +21,23 @@ comparison = schemes.map do |scheme|
   puts "\tmed. hold:   #{sells.map {|h| h[:hold] || 1000 }.median}"
   puts "\tmed. ROI:    #{sells.map {|h| h[:ROI] }.median}"
   puts "\tavg. ROI:    #{sells.map {|h| h[:ROI] }.mean}"
+  puts "\tROI / hold:  #{sells.map {|h| h[:ROI] }.mean / sells.map {|h| h[:hold] || 1000 }.median}"
 
-  prof = profit sells, :circulation => 15.0, :pieces => 30.0, :reinvest => true
+  prof = profit sells, :circulation => 15.0, :pieces => 30.0, :reinvest => true, :debug => true
   puts "\tskips:       #{prof[:skips].size}"
   puts "\tprofits:     15.0 -> #{prof[:cash]}"
   puts "\tcirculation: #{prof[:circulation]}"
 
+  puts "| #{sim.m}  | " +
+       "#{sim.b}  | " +
+       "#{sells.size}  | " +
+       "#{sells.map {|h| h[:hold] || 1000 }.median} | " +
+       "#{prof[:skips].size} | " +
+       "#{sells.map {|h| h[:ROI] }.mean} | " +
+       "#{prof[:cash]} |"
+
   prof
 end
 
-binding.pry
+#binding.pry
 
