46e08a95e34ed7b6f2fceeaf551d7ae8117361bc
Ari Brown <ari@aribrown.com>
2021-04-15 14:26:58 -0400

    fixed publish

--- script/publish.rb
+++ script/publish.rb
@@ -1,4 +1,4 @@
 require 'fileutils'
-Dir['views/*.html'].each {|f| FileUtils.mv f, "~/servers/default/public/files/#{File.basename f}" }
-Dir['docs/*.html'].each {|f| FileUtils.mv f, "~/servers/default/public/files/#{File.basename f}" }
+Dir['views/*.html'].each {|f| FileUtils.cp f, File.expand_path("~/servers/default/public/files/#{File.basename f}"), :verbose => true }
+['doc/stocks.html', 'doc/db.html'].each {|f| FileUtils.cp f, File.expand_path("~/servers/default/public/files/#{File.basename f}"), :verbose => true }
 
